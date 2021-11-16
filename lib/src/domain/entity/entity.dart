ea244bfec89bc46411858dbdb4ec1eec73eed494
Moustafa Essam <drsh_201122@yahoo.com>
2021-11-17 01:36:41 +0200

    turned stream controllers into broadcast ones

--- lib/src/domain/entity/entity.dart
+++ lib/src/domain/entity/entity.dart
@@ -1,9 +1,12 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 abstract class Entity extends Equatable {
   const Entity();
 
+  @JsonKey(ignore: true)
   get stringify => true;
 
+  @JsonKey(ignore: true)
   List<String>? get serverSuccessMessages => null;
 }
---
+++ lib/src/domain/entity/enum.dart
@@ -0,0 +1,32 @@
+import 'dart:core' as core show Enum;
+import 'dart:core' hide Enum;
+
+import 'package:api_bloc_base/src/domain/entity/entity.dart';
+import 'package:enum_to_string/enum_to_string.dart';
+
+abstract class Enum<E extends core.Enum> extends Entity {
+  late final String _name;
+  late final E? _value;
+
+  String get name => _name;
+  E? get value => _value;
+  bool get exists => _value != null;
+
+  Enum(E value)
+      : _value = value,
+        _name = EnumToString.convertToString(value);
+
+  Enum.fromString(String value, List<E> values) {
+    final existingValue = EnumToString.fromString(values, value);
+    if (existingValue == null) {
+      _value = null;
+      _name = value;
+    } else {
+      _value = existingValue;
+      _name = value;
+    }
+  }
+
+  @override
+  get props => [_name, _value];
+}
