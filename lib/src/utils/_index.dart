4728c1c2fac25179d796b730d11c1989e297e697
Moustafa Essam <drsh_201122@yahoo.com>
2022-02-15 01:07:13 +0200

    upgraded dependencies

---
+++ lib/src/utils/_index.dart
@@ -0,0 +1,3 @@
+export 'box.dart';
+export 'translatable_string.dart';
+export 'utils.dart';
---
+++ lib/src/utils/translatable_string.dart
@@ -0,0 +1,32 @@
+import 'package:intl/intl.dart';
+
+abstract class TranslatableString {
+  final String? pattern;
+  final List<String?>? args;
+  final List<TranslatableString>? translatableArgs;
+
+  const TranslatableString(this.pattern, this.args, this.translatableArgs);
+}
+
+class TranslatableText extends TranslatableString {
+  final Map<String, String?>? namedArgs;
+  final Map<String, TranslatableText>? translatableNamedArgs;
+
+  const TranslatableText(String? pattern,
+      {List<String?>? args,
+      this.namedArgs,
+      List<TranslatableString>? translatableArgs,
+      this.translatableNamedArgs})
+      : super(pattern, args, translatableArgs);
+}
+
+class TranslatableNumber extends TranslatableString {
+  final NumberFormat? format;
+  final num? number;
+
+  const TranslatableNumber(String? pattern, this.number,
+      {List<String?>? args,
+      this.format,
+      List<TranslatableText>? translatableArgs})
+      : super(pattern, args, translatableArgs);
+}
