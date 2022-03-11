e0425025e93e36bb39063ca2f4d127b393f9e7db
Moustafa Essam <drsh_201122@yahoo.com>
2022-03-12 00:14:53 +0200

    upgraded dependencies

--- lib/src/presentation/widgets/family_builder.dart
+++ lib/src/presentation/widgets/family_builder.dart
@@ -5,25 +5,49 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 typedef FamilyWidgetBuilder<Bloc extends BaseCubit> = Widget Function(
     BuildContext context, Bloc bloc);
 
-class FamilyBuilder<Arg, Bloc extends BaseCubit> extends StatefulWidget {
+class FamilyBuilder<Arg, Bloc extends BaseCubit,
+    FamilyBloc extends Family<Arg, Bloc>> extends StatelessWidget {
+  final Arg arg;
+  final Family<Arg, Bloc>? family;
+  final FamilyWidgetBuilder<Bloc> builder;
+
+  const FamilyBuilder({
+    Key? key,
+    required this.arg,
+    required this.builder,
+    this.family,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final family = this.family ?? context.read<FamilyBloc>();
+    return FamilyBlocProvider<Arg, Bloc>(
+      arg: arg,
+      family: family,
+      builder: builder,
+    );
+  }
+}
+
+class FamilyBlocProvider<Arg, Bloc extends BaseCubit> extends StatefulWidget {
   final Arg arg;
   final Family<Arg, Bloc> family;
   final FamilyWidgetBuilder<Bloc> builder;
 
-  FamilyBuilder({
+  FamilyBlocProvider({
     required this.arg,
     required this.family,
     required this.builder,
   }) : super(key: ValueKey(arg));
 
   @override
-  State<FamilyBuilder<Arg, Bloc>> createState() =>
-      _FamilyBuilderState<Arg, Bloc>();
+  State<FamilyBlocProvider<Arg, Bloc>> createState() =>
+      _FamilyBlocProviderState<Arg, Bloc>();
 }
 
-class _FamilyBuilderState<Arg, Bloc extends BaseCubit>
-    extends State<FamilyBuilder<Arg, Bloc>>
-    with FamilyListenerMixin<FamilyBuilder<Arg, Bloc>> {
+class _FamilyBlocProviderState<Arg, Bloc extends BaseCubit>
+    extends State<FamilyBlocProvider<Arg, Bloc>>
+    with FamilyListenerMixin<FamilyBlocProvider<Arg, Bloc>> {
   late final Bloc _bloc;
 
   @override
