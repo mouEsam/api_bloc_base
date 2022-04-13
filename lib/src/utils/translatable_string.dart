fccfc216e2b39f3abdd9938e39e081f47a13412b
Moustafa Essam <drsh_201122@yahoo.com>
2022-04-13 19:22:36 +0200

    upgraded dependencies

--- lib/src/utils/translatable_string.dart
+++ lib/src/utils/translatable_string.dart
@@ -2,18 +2,18 @@ import 'package:intl/intl.dart';
 
 abstract class TranslatableString {
   final String pattern;
-  final List<String?>? args;
+  final List<String>? args;
   final List<TranslatableString>? translatableArgs;
 
   const TranslatableString(this.pattern, this.args, this.translatableArgs);
 }
 
 class TranslatableText extends TranslatableString {
-  final Map<String, String?>? namedArgs;
+  final Map<String, String>? namedArgs;
   final Map<String, TranslatableText>? translatableNamedArgs;
 
   const TranslatableText(String pattern,
-      {List<String?>? args,
+      {List<String>? args,
       this.namedArgs,
       List<TranslatableString>? translatableArgs,
       this.translatableNamedArgs})
@@ -25,7 +25,7 @@ class TranslatableNumber extends TranslatableString {
   final num? number;
 
   const TranslatableNumber(String pattern, this.number,
-      {List<String?>? args,
+      {List<String>? args,
       this.format,
       List<TranslatableText>? translatableArgs})
       : super(pattern, args, translatableArgs);
